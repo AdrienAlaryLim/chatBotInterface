48b31e4f17ec7f641b507e8ac8e722bf81e63fb1
AdrienAlaryLim <adrien.alary@limayrac.fr>
2021-04-06 13:58:30 +0200

    Add id question / reponse

--- view/frontend/listCoupleWeak.php
+++ view/frontend/listCoupleWeak.php
@@ -19,8 +19,10 @@ ob_start();
       
 	<table class="tabLicence">
 			<tr>
+				<th class="tabLicence">Id Question</th>
 				<th class="tabLicence">Question</th>
-				<th class="tabLicence">Reponse</th>
+				<th class="tabLicence">Id Réponse</th>
+				<th class="tabLicence">Réponse</th>
 				<th class="tabLicence">Confiance</th>
 				<th class="tabLicence">Modifier réponse</th>
 			</tr>
@@ -30,7 +32,9 @@ ob_start();
 			{
 			?>
 				<tr>
+					<td class="tabLicence"> <?php echo $data['id_question']; ?></td>
 					<td class="tabLicence" style="width: 40%;"> <?php echo $data['question']; ?></td>
+					<td class="tabLicence"> <?php echo $data['id_reponse']; ?></td>
 					<td class="tabLicence" style="width: 40%;"> <?php echo $data['response']; ?></td>
 					<td class="tabLicence" style="width: 70px; border-right: 1px solid #ddd; text-align: center;"> <?php echo $data['confiance']; ?></td>
 					<td class="tabLicence" style="width: 110px;"> <a href=<?php echo '"index.php?action=modifierCouple&idReponse='.$data['id_reponse'].'&idQuestion='.$data['id_question'].'&conflicts='.$data['mots_cles_associes'].'"';?>> <img src="public/images/plus_36px.png" alt="Add_Response"> </a> </td>
